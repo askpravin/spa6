7583b314a69d01bf4bd7aa747a6b16ad9f11d5e4
askpravin <askpravinjadhav@gmail.com>
2025-02-13 10:27:54 +0530

    Add product detail view and update routing in App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import Home from "./components/Home";
 import Products from "./components/Products";
 import About from "./components/About";
 import { Route, Routes, Link } from "react-router-dom";
+import ProductDetail from "./components/ProductDetail";
 function App() {
   return (
     <>
       <ul>
         <li>
-          <Link to="/home" style={{ textDecoration: "none" }}>
+          <Link to="/" style={{ textDecoration: "none" }}>
             Home
           </Link>
         </li>
@@ -25,9 +26,10 @@ function App() {
       </ul>
       <hr />
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
         <Route path="/about" element={<About />} />
+        <Route path=":id" element={<ProductDetail />}></Route>
       </Routes>
     </>
   );
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,8 +1,29 @@
+import { Link, Outlet } from "react-router-dom";
 
 const Products = () => {
   return (
-    <div>Products</div>
-  )
-}
+    <div>
+      {" "}
+      <ul>
+        <li>
+          <Link to="/product1" style={{ textDecoration: "none" }}>
+            Apple
+          </Link>
+        </li>
+        <li>
+          <Link to="/product2" style={{ textDecoration: "none" }}>
+            Banana
+          </Link>
+        </li>
+        <li>
+          <Link to="/product3" style={{ textDecoration: "none" }}>
+            Orange{" "}
+          </Link>
+        </li>
+      </ul>
+      <Outlet />
+    </div>
+  );
+};
 
-export default Products
+export default Products;
